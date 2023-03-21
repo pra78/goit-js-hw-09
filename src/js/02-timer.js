529eafc09a001e0f307d0f2a1189f817fe9f594b
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-21 20:51:40 +0200

    task 2 css styles

---
+++ src/js/02-timer.js
@@ -0,0 +1,62 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+const buttonStart = document.querySelector('[data-start]');
+const clockFace = document.querySelectorAll('.field .value')
+
+
+clockFace[0].dataset.days.value = 15;
+
+buttonStart.addEventListener('click', onStartButtonClick)
+
+buttonStart.disabled = true;
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (selectedDates[0] <= Date.now()) {
+            window.alert("Please choose a date in the future");
+        } else {
+            buttonStart.disabled = false;
+        }
+    },
+};
+
+const fp = flatpickr("#datetime-picker", { ...options });
+
+function onStartButtonClick() {
+    console.log(fp.selectedDates[0] - Date.now())
+}
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value) {
+
+}
