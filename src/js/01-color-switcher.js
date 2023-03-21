85e7f00e5e0fcc4c631a2f35e0da883df15421a5
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-21 15:51:12 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 
+buttonStop.disabled = true;
+let intervalId = null;
+
+buttonStart.addEventListener('click', onStartButtonClick);
+buttonStop.addEventListener('click', onStopButtonClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onStartButtonClick() { 
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+};
+
+function onStopButtonClick() { 
+  clearInterval(intervalId);
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+};
