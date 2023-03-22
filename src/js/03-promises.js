868b645dbe759a536b765bad3af4166f35dcd532
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-22 21:07:15 +0200

    task 3 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('form');
+
+const createPromisesBtn = document.querySelector('button');
+
+createPromisesBtn.addEventListener('click', onSubmitButtonPressed);
+
+function onSubmitButtonPressed(event) {
+  event.preventDefault();
+  
+  let position = 0;
+  let delay = Number(form.delay.value);
+  const step = Number(form.step.value);
+  let amount = Number(form.amount.value);
+
+  while (amount > 0) {
+    position += 1;
+    createPromise(position, delay)
+      .then(result => Notify.success(result))
+      .catch(error => Notify.failure(error));
+    delay += step;
+    amount -= 1;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
